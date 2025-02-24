e2618811b3d762ede9daeb0674fea737f9ce0b12
Rishit Butola <rishitbutola10@gmail.com>
2025-02-24 17:14:08 +0530

    timepass

---
+++ C-easy/add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+    a = 5;
+    b = 4;
+    sum = a + b;
+    
+    printf("Sum: %d\n", sum); // Corrected printf statement
+
+    return 0;
+}
